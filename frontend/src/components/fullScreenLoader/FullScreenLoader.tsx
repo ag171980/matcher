4eac46f95cc4c027bb08978927e0d2edf6baad57
Alexis Gutiérrez <agutierrez@tecnoaccion.corp>
2022-12-05 16:27:30 -0300

    a'

--- frontend/src/components/fullScreenLoader/FullScreenLoader.tsx
+++ frontend/src/components/fullScreenLoader/FullScreenLoader.tsx
@@ -1,7 +1,7 @@
 import logo from '../../assets/logoLoader.svg';
 
 const FullScreenLoader = () => {
-    return ( 
+    return (
         <div className='gradientBg h-screen w-screen flex flex-col items-center justify-center'>
             <img src={logo} alt="Matcher logo" className='w-60 sm:w-64 block loadingLogo' />
         </div>
