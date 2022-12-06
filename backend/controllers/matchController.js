18c3d4218b98affb11f535deb6d00a83739508ee
Alexis Gutiérrez <agutierrez@tecnoaccion.corp>
2022-12-06 16:24:34 -0300

    a

--- backend/controllers/matchController.js
+++ backend/controllers/matchController.js
@@ -3,12 +3,19 @@
 
 import MatchModel from "../models/MatchModel.js";
 import UserModel from "../models/UserModel.js";
-
+import  { Op } from "sequelize";
 /* METODOS DEL CRUD */
 
 
 //campos
 
+// const eliminarItemEspecifico = async (nombre, arr)=>{
+//     console.log(arr)
+//     let indexOf = arr.indexOf(nombre)
+//     console.log(nombre)
+//     console.log(indexOf)
+//     arr.splice(indexOf, 1)
+// }
 
 //MOSTRAR TODOS LOS REGISTROS
 export const getAllMatches = async (req, res) => {
@@ -29,25 +36,41 @@ export const getMatchById = async (req, res) => {
 }
 
 export const verifyMatchesUserById = async (req, res)=>{
-    let idUserEntry = 1;
+    let idUserEntry = req.query.idUser;
     try{
-        const usersLikedsByEntry = await UserModel.findAll({where:{id: idUserEntry}});
+        const users = await UserModel.findAll();
         const allMatches = await MatchModel.findAll();
+        const userActual = await UserModel.findAll({where:{id: idUserEntry}})
+        const filterMatchesById = await MatchModel.findAll({where: 
+            {[Op.or]: [
+                    {
+                        id_user_matchA: {[Op.eq]: Number(idUserEntry)}
+                    }, 
+                    {
+                        id_user_matchB: {[Op.eq]: Number(idUserEntry)}
+                    }
+                ]
+            }
+            })
+        // let filterMatchesById = allMatches.filter((match)=>match.id_user_matchA === idUserEntry || match.id_user_matchB === idUserEntry)
         let usersMatches = []
-        // allMatches.map((match, index)=>{
-        //     let idActualA = match.id_user_matchA;
-        //     let idActualB = match.id_user_matchB;
-        //     let userByIdA = users.filter((us)=>us.id === idActualA)[0].name
-        //     let userByIdB = users.filter((us)=>us.id === idActualB)[0].name
-        //     allMatches.map((matchJ, jndex)=>{
-        //         if(matchJ.id_user_matchB === idActualA && matchJ.id_user_matchA === idActualB){
-        //             console.log(`${userByIdA} y ${userByIdB} hicieron match!`)
-        //         }
-        //     })
+        
+        filterMatchesById.map((match, index)=>{
+            let idActualA = match.id_user_matchA;
+            let idActualB = match.id_user_matchB;
+            let userByIdB = users.filter((us)=>us.id === idActualB)[0].name
+            filterMatchesById.map((matchJ, jndex)=>{
+                if(matchJ.id_user_matchB === idActualA && matchJ.id_user_matchA === idActualB){
+                    if(userByIdB !== userActual[0].name){
+                        let dataUserToMatch = users.filter((user)=>user.name === userByIdB)
+                        usersMatches.push(dataUserToMatch)
+                    }
+                    // eliminarItemEspecifico(userActual, usersMatches)
+                }
+            })
 
-        // })
-
-        res.json(usersLikedsByEntry)
+        })
+        res.json(usersMatches)
     }catch(error){
         res.json({message: error.message })
     }
